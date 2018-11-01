d2c166a918b38aa23f973034c66066ae2aba46f2
irabespodobna <penkovskaya.irina@gmail.com>
2018-11-01 20:24:35 +0100

    added sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  
-  
+
+
 	root to: 'static_page#home'
 	# get '/', to: 'static_page#home'
-  resources :gossips do 
+  resources :gossips do
   	resources :comments
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/gossips/:id', to: 'comments#create'
+  resources :users 
+
+  # post '/gossips/:id', to: 'comments#create'
+  # get '/users/new', to: 'users#new'
+  # post '/users', to: 'users#create'
+  # get '/users', to: 'users#index'
 
 end
