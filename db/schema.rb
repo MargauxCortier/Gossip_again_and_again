2fbb9b16e537c7dbd4164fc1265b9b0f43c875c5
irabespodobna <penkovskaya.irina@gmail.com>
2018-11-01 19:18:50 +0100

    push push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_183008) do
+ActiveRecord::Schema.define(version: 2018_11_01_181346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2018_10_31_183008) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
   add_foreign_key "comments", "gossips"
